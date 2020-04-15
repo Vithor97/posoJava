e3eaa6a33988b3e6ff6e8fab05e8b8183ed9dc5c
Carol <Carol@Lenovo-PC>
2020-04-15 13:37:10 -0300

    Criacao da classe DetalharPessoaBean

---
+++ src/br/com/bradesco/posoTeatro/view/bean/DetalharPessoaBean.java
@@ -0,0 +1,60 @@
+package br.com.bradesco.posoTeatro.view.bean;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+
+import br.com.bradesco.posoTeatro.model.Pessoa;
+
+
+@ManagedBean(name="consultarPessoaDetalheBean")
+@SessionScoped
+public class DetalharPessoaBean extends PosoBean implements BeanInterface {
+	
+	private Pessoa pessoa;
+	private String mensagem;
+	
+	@ManagedProperty(value = "#{alteraPessoaBean}")
+	private AlteraPessoaBean alteraPessoaBean;
+	
+	
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+	public AlteraPessoaBean getAlteraPessoaBean() {
+		return alteraPessoaBean;
+	}
+	public void setAlteraPessoaBean(AlteraPessoaBean alteraPessoaBean) {
+		this.alteraPessoaBean = alteraPessoaBean;
+	}
+	@Override
+	public String iniciarPagina(List<String> titulosBread, List<String> urlsBread) {
+		super.iniciarPagina("Pessoas Detalhe", "detalhePessoas", titulosBread, urlsBread);
+		return "detalhePessoas";
+	}
+	@Override
+	public String iniciarPagina() {
+		super.iniciarPagina("Pessoas Detalhe", "detalhePessoas");
+		return "detalhePessoas";
+	}
+	
+	public String alteracao() {
+		alteraPessoaBean.setPessoa(pessoa);
+		return alteraPessoaBean.iniciarPagina();
+	}
+	
+	
+
+}
