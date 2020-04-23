a3d986bd7759669b84202f2296978fa788991787
Carol <Carol@Lenovo-PC>
2020-04-23 17:20:18 -0300

    Alteração nas classes alterarEvento.xhtml                       listarEvento.xhtml                       EventoDao                       listarEventosBean                       alterarEventos Alterar e exluir funcionando

--- src/br/com/bradesco/posoTeatro/view/bean/evento/ListarEventosBean.java
+++ src/br/com/bradesco/posoTeatro/view/bean/evento/ListarEventosBean.java
@@ -23,10 +23,8 @@ public class ListarEventosBean extends PosoBean implements BeanInterface{
 	@ManagedProperty(value = "#{alterarEventoBean}")
 	private AlterarEventoBean alterarEventoBean;
 	
+	private EventoDao dao;
 	private Evento evento;
-	
-
-
 	private String mensagem;
 	private ArrayList<Evento> eventos;
 	private String pesquisaEvento = "";
@@ -66,9 +64,17 @@ public class ListarEventosBean extends PosoBean implements BeanInterface{
 	public void setEvento(Evento evento) {
 		this.evento = evento;
 	}
+	
+	public EventoDao getDao() {
+		return dao;
+	}
+
+	public void setDao(EventoDao dao) {
+		this.dao = dao;
+	}
 
 	
-	public AlterarEventoBean getAlteraPessoaBean() {
+	public AlterarEventoBean getAlterarEventoBean() {
 		return alterarEventoBean;
 	}
 	
@@ -88,8 +94,6 @@ public class ListarEventosBean extends PosoBean implements BeanInterface{
 		return "listarEventos";
 	}
 	
-
-	
 	public void pesquisarEventos() {
 		setEventos(new EventoDao().listar(getPesquisaEvento()));
 	}
@@ -100,12 +104,33 @@ public class ListarEventosBean extends PosoBean implements BeanInterface{
 		return detalheEventoBean.iniciarPagina(getTitulosBread(), getUrlsBread());
 	}
  	
- 
-	
-	public String alteracao() {
-		alterarEventoBean.setEvento(evento);
+	public String alteracao(Evento evento) {
+		alterarEventoBean.setEvento(new EventoDao().consultar(evento));
+		alterarEventoBean.setTelaAnterior(this);
 		return alterarEventoBean.iniciarPagina();
 	}
 
-
+	public void excluir(Evento evento) {
+		System.out.println("Dentro do metodo de exclusão");
+		int RowAffected = 0;
+		
+		RowAffected = new EventoDao().excluirEvento(evento);
+		evento = new EventoDao().consultar(evento);
+		if(RowAffected !=0) {
+			if(evento.getSituacaoEvento() !=1) {				
+				setMensagem("Exclusão realizada");
+				setEventos(new EventoDao().listar(getPesquisaEvento()));
+			}
+			else {
+				
+			}
+		}
+		else {
+			setMensagem("Exclusão não realizada");
+			
+		}
+		
+	}
+	
 }
+
